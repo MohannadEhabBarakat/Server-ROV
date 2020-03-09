d98d3f3523bd177f05647ee545d24c5bff19cedb
MohannadEhabBarakat <hannod98@yahoo.com>
2020-03-09 10:01:25 +0200

    base code

--- app/main.py
+++ app/main.py
@@ -3,6 +3,29 @@ from flask import Flask, request
 app = Flask(__name__)
 
 connect=False
+im
+
+@app.route('/')
+def index():
+    return "Hi this is the ROV winner's server :) just have fun"
+
+def get_frame():
+    camera_port=0
+    camera=cv2.VideoCapture(camera_port) #this makes a web cam object
+
+    while True:
+        retval, im = camera.read()
+        imgencode=cv2.imencode('.jpg',im)[1]
+        stringData=imgencode.tostring()
+        yield (b'--frame\r\n'
+            b'Content-Type: text/plain\r\n\r\n'+stringData+b'\r\n')
+
+    del(camera)
+
+@app.route('/camera1')
+def camera1():
+     return Response(get_frame(),mimetype='multipart/x-mixed-replace; boundary=frame')
+
 
 @app.route('/joy')
 def joy():
